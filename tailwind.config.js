a40b322604b7c766d08c6fc762712814a301af5f
jeffreyai7 <jirukeh@gmail.com>
2024-11-21 11:17:16 +0100

    finish tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html", // Include all HTML files in the root directory
+    "./src/**/*.{js,ts,jsx,tsx}", // Include any JS/TS/React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
